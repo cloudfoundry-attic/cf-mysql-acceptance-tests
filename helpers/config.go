01120e1bd0e64fa25f94831fe0cb0d461af322fa
Rob Dimsdale-Zucker <rdimsdale@pivotal.io>
2016-12-16 09:05:04 -0800

    Use name instead of plan_name [#136235719]

--- helpers/config.go
+++ helpers/config.go
@@ -13,7 +13,7 @@ type Component struct {
 }
 
 type Plan struct {
-	Name               string `json:"plan_name"`
+	Name               string `json:"name"`
 	MaxStorageMb       int    `json:"max_storage_mb"`
 	MaxUserConnections int    `json:"max_user_connections"`
 	Private            bool   `json:"private,omitempty"`
